e7f3c90e67548ac3992b5b5271ea04635a57e872
David <davo00771339@gmail.com>
2023-10-04 21:11:03 +0400

    Some Changes 4

--- src/components/Gridbox/GridBoxes.js
+++ src/components/Gridbox/GridBoxes.js
@@ -5,7 +5,7 @@ const GridBoxes = () => {
   return (
     <div className="grid-container">
       <div className="grid-box">Boxx 1</div>
-      <div className="grid-box">Box 2</div>
+      <div className="grid-box">Bo1x 2</div>
       <div className="grid-box">Box 3</div>
       <div className="grid-box">Box 4</div>
     </div>
