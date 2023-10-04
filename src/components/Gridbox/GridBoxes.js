ca1adadee50a99627db91090f3e3e4c5ca79cf9f
David <davo00771339@gmail.com>
2023-10-04 19:46:21 +0400

    Cart

--- src/components/Gridbox/GridBoxes.js
+++ src/components/Gridbox/GridBoxes.js
@@ -4,7 +4,7 @@ import './GridBoxes.css'; // Import your CSS file
 const GridBoxes = () => {
   return (
     <div className="grid-container">
-      <div className="grid-box">Box 1</div>
+      <div className="grid-box">Boxx 1</div>
       <div className="grid-box">Box 2</div>
       <div className="grid-box">Box 3</div>
       <div className="grid-box">Box 4</div>
